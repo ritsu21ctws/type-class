d13d7f3fdb5c34d8ed4206544cb04c6060d8f31a
ritsu <ritsu.21.ctws@gmail.com>
2024-12-16 00:21:19 +0900

    データ一覧を表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import { Button } from "@/components/ui/button"
-import { HStack } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import { Table } from "@chakra-ui/react"
+import { GetAllTodos } from "@/lib/todo"
+import { Todo } from "@/domain/todo"
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const getAllTodos = async () => {
+      const todoData =  await GetAllTodos();
+      setTodos(todoData);
+    }
+
+    getAllTodos();
+  }, [])
+
   return (
     <>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
+      <Table.Root size="sm">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>Title</Table.ColumnHeader>
+            <Table.ColumnHeader>Done</Table.ColumnHeader>
+            <Table.ColumnHeader>CreatedAt</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {todos.map((todo) => (
+            <Table.Row key={todo.id}>
+              <Table.Cell>{todo.title}</Table.Cell>
+              <Table.Cell>{todo.done ? "TRUE" : "FALSE"}</Table.Cell>
+              <Table.Cell>{todo.created_at}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </>
   )
 }
